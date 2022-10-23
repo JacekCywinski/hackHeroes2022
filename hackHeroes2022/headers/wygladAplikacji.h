b8f4d908fcd6e1761534640f4866afcaf3bd4897
JacekCywinski <116505826+JacekCywinski@users.noreply.github.com>
2022-10-23 19:39:59 +0200

    Add files via upload

---
+++ hackHeroes2022/headers/wygladAplikacji.h
@@ -0,0 +1,18 @@
+#ifndef WYGLADAPLIKACJI_H
+#define WYGLADAPLIKACJI_H
+
+#include "funkcjeKlasy.h"
+
+// prototypy funkcji
+
+void ekranLogowania();
+
+void menuGlowne(Konto &konto);
+
+void brakZbiorek(Konto &konto);
+
+void wyswietlZbiorki(Konto &kontoInfo);
+
+void profil(Konto &konto);
+
+#endif
